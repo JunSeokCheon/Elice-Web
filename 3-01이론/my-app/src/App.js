70b8d1007fa7d75d527c14516a10027435648014
JunSeokCheon <goosw775@naver.com>
2022-03-01 20:56:09 +0900

    3-01이론

---
+++ 3-01이론/my-app/src/App.js
@@ -0,0 +1,79 @@
+import './App.css';
+import {useState} from 'react';
+
+
+function Header(props){
+  function clickHandler(event){
+    event.preventDefault();
+    props.onChangeMode();
+  }
+  return <header>
+    <h1><a href="/" onClick={clickHandler}>WEB</a></h1>
+  </header>
+}
+function Nav(props){
+  function clickHandler(event){
+    event.preventDefault();
+    props.onChangeMode(Number(event.target.dataset.id));
+  }
+  let lis = [];
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a href={'/read/'+t.id} data-id={t.id} onClick={clickHandler}>{t.title}</a>
+    </li>)
+  }
+  return <nav>
+    <ol>
+        {lis}
+    </ol>
+  </nav>
+}
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
+function App() {
+  let topics = [
+    {id:1, title:'HTML', body:'HTML is ...'},
+    {id:2, title:'CSS', body:'CSS is ...'},
+    {id:3, title:'JavaScript', body:'JavaScript is ...'}
+  ];
+  let [mode, setMode] = useState('WELCOME');
+  let [id, setId] = useState(null);
+  function onChangeHeaderHandler(){
+    setMode('WELCOME');
+  }
+  function onChangeNavHandler(id){
+    setMode('READ');
+    setId(id);
+  }
+  let articleTag = '';
+  if(mode === 'WELCOME'){
+    articleTag = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if(mode === 'READ'){
+    let title = '';
+    let body = '';
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+        break;
+      }
+    }
+    articleTag = <Article title={title} body={body}></Article>
+  }
+  return (
+        <> 
+          <Header onChangeMode={onChangeHeaderHandler}></Header>
+          <Nav topics={topics} onChangeMode={onChangeNavHandler}></Nav>
+          {articleTag}
+        </>
+  );
+}
+
+
+export default App;
